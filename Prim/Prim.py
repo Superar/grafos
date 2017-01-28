8447a5c4fb82124286a9603860c661386a27b957
Marcio Lima <marcio.lima.inacio@gmail.com>
2017-01-28 17:04:50 -0200

    Uso de fila de prioridades para maior eficiência

--- Prim/Prim.py
+++ Prim/Prim.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8
 import networkx as nx
 import matplotlib.pyplot as plt
-import operator
 import sys
+import Queue
 
 
 def prim_mst(grafo, r):
@@ -13,24 +13,19 @@ def prim_mst(grafo, r):
     grafo.node[r]['lam'] = 0
 
     # Criação da fila de prioridades
-    fila_q = sorted(grafo.node.items(),
-                    key=lambda k: operator.itemgetter('lam')(
-                        operator.itemgetter(1)(k)))  # Ordena lista de acordo com o valor de lambda
-    fila_q = [x[0] for x in fila_q]  # Lista possui os nomes dos vértices
+    fila_q = Queue.PriorityQueue()
+
+    for no in grafo.node.items():
+        fila_q.put((no[1]['lam'], no[0]))
 
     # Algoritmo de Prim
-    while len(fila_q) > 0:
-        u = fila_q.pop(0)
+        u = fila_q.get()[1]
+        lista_fila = [x[1] for x in fila_q.queue]
 
         for v in nx.all_neighbors(grafo, u):
-            if v in fila_q and grafo.node[v]['lam'] > grafo.edge[u][v]['weight']:
+            if v in lista_fila and grafo.node[v]['lam'] > grafo.edge[u][v]['weight']:
                 grafo.node[v]['lam'] = grafo.edge[u][v]['weight']
                 grafo.node[v]['pred'] = u
-
-        fila_q = sorted(grafo.node.items(),
-                        key=lambda k: operator.itemgetter('lam')(
-                            operator.itemgetter(1)(k)))
-        fila_q = [x[0] for x in fila_q]
 
     # Criação do grafo da árvore
     g_out = nx.Graph()
@@ -48,16 +43,17 @@ def main():
     print('Criando MST...')
     g_out = prim_mst(g_in, 0)
 
-    print('Desenhando árvore...')
-    pos = nx.spring_layout(g_out, k=0.15, iterations=50)
-    nx.draw_networkx_nodes(g_out, pos, cmap=plt.get_cmap('jet'), node_size=150)
-    nx.draw_networkx_edges(g_out, pos, edgelist=g_out.edges(), arrows=True)
-    nx.draw_networkx_labels(g_out, pos, font_size=5)
-    labels = nx.get_edge_attributes(g_out, 'weight')
-    nx.draw_networkx_edge_labels(g_out, pos, edge_labels=labels, font_size=5)
-    plt.axis('off')
+    if len(sys.argv) == 3 and sys.argv[2] == '-d':
+        print('Desenhando árvore...')
+        pos = nx.spring_layout(g_out, k=0.15, iterations=50)
+        nx.draw_networkx_nodes(g_out, pos, cmap=plt.get_cmap('jet'), node_size=150)
+        nx.draw_networkx_edges(g_out, pos, edgelist=g_out.edges(), arrows=True)
+        nx.draw_networkx_labels(g_out, pos, font_size=5)
+        labels = nx.get_edge_attributes(g_out, 'weight')
+        nx.draw_networkx_edge_labels(g_out, pos, edge_labels=labels, font_size=5)
+        plt.axis('off')
 
-    plt.savefig('mst_prim.pdf')
+        plt.savefig('mst_prim.pdf')
 
 
 if __name__ == "__main__":
