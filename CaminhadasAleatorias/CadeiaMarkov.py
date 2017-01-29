1871bed6102f8442c89dad6eea5c1481d6b3f576
Marcio Lima <marcio.lima.inacio@gmail.com>
2017-01-29 12:38:21 -0200

    Eliminação de funções não utilizadas e saída arrumada

--- CaminhadasAleatorias/CadeiaMarkov.py
+++ CaminhadasAleatorias/CadeiaMarkov.py
@@ -1,15 +1,18 @@
+# -*- coding: UTF-8 -*-
 import networkx as nx
 import numpy as np
 
 
+# Distribuição teorica das probabilidades
 def distribuicao_teorica(grafo):
     w = np.ones(grafo.number_of_nodes())
-    w = w * grafo.in_degree().values()
-    w = w / (2 * grafo.number_of_edges())
+    w *= grafo.in_degree().values()
+    w /= 2 * grafo.number_of_edges()
 
     return w
 
 
+# Criação da matriz de probabilidades
 def probabilidades(grafo):
     dimensao = grafo.number_of_nodes()
     matriz_p = nx.convert_matrix.to_numpy_matrix(grafo)
@@ -24,33 +27,20 @@ def probabilidades(grafo):
     return delta * matriz_p
 
 
-def power_method(estado_anterior, grafo):
-    matriz_p = np.matrix(probabilidades(grafo)).transpose()
-
-    return np.matrix(estado_anterior) * matriz_p
-
-
-def caminhada(estado_inicial, grafo, k):
-    estado_atual = estado_inicial
-
-    for i in range(k):
-        estado_atual = power_method(estado_atual, grafo)
-
-    return np.squeeze(np.asarray(estado_atual))#
-
+# Caminhada aleatória com Power Method
 def caminhada_d(estado_inicial, grafo, k):
     p = np.matrix(probabilidades(grafo))
     aux = p.copy()
 
     for i in range(k):
-        aux = np.dot(aux,p)
+        aux = np.dot(aux, p)
 
     return estado_inicial * aux
 
 
 def main():
-    G = nx.DiGraph()
-    G.add_nodes_from([x for x in range(1, 37)])
+    g = nx.DiGraph()
+    g.add_nodes_from([x for x in range(1, 37)])
     arestas = [(1, 15), (1, 3), (3, 4), (3, 7), (4, 7), (4, 6),
                (6, 7), (6, 8), (7, 8), (7, 27), (8, 27), (8, 10), (10, 11),
                (10, 12), (11, 12), (11, 13), (12, 13), (12, 14), (13, 14), (13, 15),
@@ -59,12 +49,17 @@ def main():
                (22, 16), (23, 16), (23, 35), (26, 27), (26, 28),
                (27, 28), (27, 29), (28, 29), (28, 30), (29, 30), (29, 31), (30, 31),
                (31, 30), (31, 33), (33, 12), (33, 35), (34, 34), (35, 36)]
-    G.add_edges_from(arestas)
+    g.add_edges_from(arestas)
 
-    estado_inicial = np.array([0. for i in range(36)])
+    estado_inicial = np.zeros(36)
     estado_inicial[0] = 1.0
 
-    print(distribuicao_teorica(G))
+    print('Distribuição teórica')
+    print(distribuicao_teorica(g))
+
+    print('\nDistribuição pelo método da potência')
+    print(caminhada_d(estado_inicial, g, 100))
+
 
 if __name__ == "__main__":
     main()
